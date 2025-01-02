bd37010600c50e4132c2e35338b9d2d1fafceb9d
Marjorie SIAD <marjoriesiad@gmail.com>
2025-01-02 21:31:55 +0100

    :lipstick: create an add product button on products page

--- src/app/produits/page.jsx
+++ src/app/produits/page.jsx
@@ -1,19 +1,22 @@
 import Navbar from "@/components/Navbar";
 import ProductListItem from "@/components/ProductListItem";
 
-export default function Produits(){
-    return (
-        <>
-            <Navbar />
-            <div className="flex flex-col items-center gap-3 mt-8">
-                <ProductListItem />
-                <ProductListItem />
-                <ProductListItem />
-                <ProductListItem />
-                <ProductListItem />
-                <ProductListItem />
-                <ProductListItem />
-            </div>
-        </>
-    )
+export default function Produits() {
+  return (
+    <>
+      <Navbar />
+      <div className="flex flex-col items-center gap-3">
+        <div className="bg-green-900 flex justify-center items-center h-10 w-[95%] p-2 rounded-lg font-bold">
+          Ajouter un produit
+        </div>
+        <ProductListItem />
+        <ProductListItem />
+        <ProductListItem />
+        <ProductListItem />
+        <ProductListItem />
+        <ProductListItem />
+        <ProductListItem />
+      </div>
+    </>
+  );
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,22 +3,37 @@ import { LuCroissant, LuLogOut } from "react-icons/lu";
 
 export default function Navbar() {
   return (
-    <div className="flex justify-between items-center p-4 bg-gray-800">
-      <div className="">
-        <p className="md:flex items-center gap-1 uppercase text-lg font-bold hidden">
-          Culture Terville
-        </p>
-        <p className="flex items-center md:hidden text-xl gap-1"><LuCroissant/> CT</p>
-      </div>
-      <nav className="flex gap-4 text-lg items-center">
-        <Link href="/">Accueil</Link>
-        <Link href="/">Clients</Link>
-        <Link href="/">Produits</Link>
-        <Link href="/">Ventes</Link>
-        <Link href="/">Outils</Link>
-        <Link href="/"><LuLogOut /></Link>
-      </nav>
+<div className="navbar bg-base-100 z-10 md:px-10">
+    <div className="flex-1">
+      <a className="btn btn-ghost text-xl">Culture Pain <LuCroissant /></a>
     </div>
+    <div className="flex-none">
+      <ul className="menu menu-horizontal px-1 md:hidden">
+        <li>
+          <details>
+            <summary>Menu</summary>
+            <ul className="bg-base-100 rounded-t-none p-2 z-50">
+              <li><Link href="/">Clients</Link></li>
+              <li><Link href="/produits">Produits</Link></li>
+              <li><Link href="/">Ventes</Link></li>
+              <li><Link href="/">Chiffres</Link></li>
+              <li><Link href="/outils">Outils</Link></li>
+              <li><Link href="/"><LuLogOut /></Link></li>
+            </ul>
+          </details>
+        </li>
+      </ul>
+
+      <ul className="hidden md:flex gap-4 text-lg items-center">
+        <li><Link href="/">Accueil</Link></li>
+        <li><Link href="/">Clients</Link></li>
+        <li><Link href="/">Produits</Link></li>
+        <li><Link href="/">Ventes</Link></li>
+        <li><Link href="/">Outils</Link></li>
+        <li><Link href="/"><LuLogOut /></Link></li>
+      </ul>
+    </div>
+  </div>
     
   );
 }
